03588d5d07bf43f762a077cb6fb99174603c0cd2
46134167 <108168175+46134167@users.noreply.github.com>
2024-01-04 19:42:25 +1100

    Add files via upload

--- src/components/PublicationComponent.vue
+++ src/components/PublicationComponent.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="innercontainer" style="width:98%;height:100%;margin:0 auto">
       <div id="Welcome" align="left" >
+          <p style="font-size:25px;margin-top:10px;">2024</p>
+          <p style="font-size:17px;margin-top:-10px;">
+              Tianle Yang & Phil Rose. (2024). Left dominant or right dominant? Problematic sandhi types in the Chinese Wu dialect of Jinshan.
+              <a href='XXX'>[2024 LSA Annual Meeting] </a>
+              <a href='/dominance_poster_LSA.pdf'>[poster]</a>
+
+          </p>
           <p style="font-size:25px;margin-top:10px;">2022</p>
           <p style="font-size:17px;margin-top:-10px;">
               Phil Rose & Tianle Yang. (2022). Modeling Interaction between Tone and Phonation Type in the Northern Wu Dialect of Jinshan.
-              <a href='XXX'>[peer-reviewed and accepted by the 18th Australasian International Conference on Speech Science and Technology] </a>
-              <a href='/Jinshan_SST.pdf'>[click here to preview]</a>
+              <a href='XXX'>[18th Australasian International Conference on Speech Science and Technology] </a>
+              <a href='/Jinshan_SST.pdf'>[inproceedings]</a>
 
           </p>
 
@@ -13,7 +20,6 @@
               Tianle Yang. (2022). Neural networks with different initialization methods for depression detection.
               <a href='https://doi.org/10.48550/arXiv.2205.04792'>[https://doi.org/10.48550/arXiv.2205.04792] </a> 
               <a href='https://arxiv.org/pdf/2205.04792.pdf'>[preprint]</a>
-
           </p>
 
           <p style="font-size:25px;margin-top:10px;">Manuscripts</p>
